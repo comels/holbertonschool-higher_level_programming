7784107e2c97568bd52ad659803f5ff0f9e1f987
comels <come.l.s@hotmail.fr>
2022-04-28 11:40:57 +0000

    modif modif name

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 
-total = 0
-for num in range(1, len(argv)):
-    num = int(argv[num])
-    total += num
-print("{}".format(total))
-
 if __name__ == "__main__":
-    argv
+    total = 0
+    for num in range(1, len(argv)):
+        num = int(argv[num])
+        total += num
+    print("{}".format(total))
