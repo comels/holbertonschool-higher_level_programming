91983ce88e25966b4eb0c498f34014b67ff5fc62
comels <come.l.s@hotmail.fr>
2022-04-28 13:20:25 +0000

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
 
     elif argv[2] not in ["+", "-", "/", "*"]:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
 
     else:
         a = int(argv[1])
