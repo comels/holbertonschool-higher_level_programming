cb4d9cc0163231e911744a52dbe76c1a72166e5a
comels <come.l.s@hotmail.fr>
2022-04-28 10:01:21 +0000

    calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,3 +7,6 @@ print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
 print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
 print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
 print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+
+if __name__ == "__main__":
+    calculator_1
