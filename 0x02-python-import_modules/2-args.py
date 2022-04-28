0d8fbae6f7e8cc4274141d56d4bbc0234792b91c
comels <come.l.s@hotmail.fr>
2022-04-28 11:32:23 +0000

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-i = 1
-if len(argv) < 2:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(argv) - 1))
-    for argument in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
-        i += 1
-
 if __name__ == "__main__":
-    argv
+    i = 1
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("{}: {}".format(i, argv[i]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for argument in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
