2293b6677b45ffe432366c52bc96ea51029f0276
comels <come.l.s@hotmail.fr>
2022-05-02 12:32:51 +0000

    palin

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * is_palindrome - function that check is the list is a palindrome
+ * @head: the list to check
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int i = 0, j = 0, k;
+  listint_t *tmp = *head, *tmp2 = *head;
+
+  while (tmp->next->next)
+  {
+    tmp = tmp->next;
+    i++;
+  }
+  k = i / 2;
+  if (i % 2 != 0)
+    j--;
+  for(; j < k + 1; j++)
+  {
+    tmp = *head;
+    for(i = k * 2; i - j != 0; i--)
+    {
+      tmp = tmp->next;
+    }  
+    if (tmp->next->n != tmp2->n)
+    {
+      return (0);
+    }
+    tmp2 = tmp2->next;
+  }
+  return (1);
+}
