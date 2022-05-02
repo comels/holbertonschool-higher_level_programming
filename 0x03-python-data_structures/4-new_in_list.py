ffe171377dcf40913affa9beff710cc08a03a195
comels <come.l.s@hotmail.fr>
2022-05-02 10:34:59 +0000

    if

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if not 0 < idx < len(my_list):
+    if not 0 <= idx < len(my_list):
         return my_list
     new_list = my_list[:]
     new_list[idx] = element
