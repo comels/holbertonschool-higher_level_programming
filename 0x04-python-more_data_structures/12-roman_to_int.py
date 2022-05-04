833c1e295648f3a0e7620dffeb85b92e3e2f3602
comels <come.l.s@hotmail.fr>
2022-05-04 12:21:58 +0000

    romain

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,29 +3,22 @@ def roman_to_int(roman_string):
     if type(roman_string)==str:
         sum = 0
         for letter in roman_string:
+            if letter not in ['I', 'V', 'X', 'L', 'C', 'D', 'M']:
+                return 0
             if letter == "I":
                 sum += 1
-                continue
             if letter == "V":
                 sum += 5
-                continue
             if letter == "X":
                 sum += 10
-                continue
             if letter == "L":
                 sum += 50
-                continue
             if letter == "C":
                 sum += 100
-                continue
             if letter == "D":
                 sum += 500
-                continue
             if letter == "M":
                 sum += 1000
-                continue
-            else:
-                return 0
         return sum
-    else: 
+    else:
         return 0
