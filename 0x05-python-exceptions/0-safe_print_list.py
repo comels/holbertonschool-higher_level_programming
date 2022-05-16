46408b7861eb0956a4e5881f60c08769af136399
comels <come.l.s@hotmail.fr>
2022-05-16 10:21:48 +0000

    except error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             num_print += 1
-        except:
+        except IndexError:
             break
     print()
     return num_print
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+from decimal import DivisionByZero
 import re
 
 
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except DivisionByZero:
         result = None
     finally:
         print("Inside result: {}".format(result))
