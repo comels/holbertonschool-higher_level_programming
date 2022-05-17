5777561f94c6481cc613b2be4c215c6de0972b46
comels <come.l.s@hotmail.fr>
2022-05-17 14:01:41 +0000

    second commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,6 +14,16 @@ class Square:
         self.__size = size
         self.__position = position
 
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        if type(position) is not tuple or len(position) != 2 or \
+           type(position[0]) is not int or position[0] < 0 or \
+           type(position[1]) is not int or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         """Calculate the current square area
 
