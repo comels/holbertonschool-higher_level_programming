d5068f863603192bfe5fa3368ed410f24bdce365
comels <come.l.s@hotmail.fr>
2022-05-20 17:38:31 +0000

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,16 @@ def text_indentation(text):
     if isinstance(text, str) is False:
         raise TypeError("text must be a string")
     else:
+        i = 0
         for char in text:
-            print("{}".format(char), end="")
-            if char in [".", "?", ":"]:
-                print("\n")
+            if i == 0:
+                if char == " ":
+                    continue
+                else:
+                    i = 1
+            if i == 1:
+                if char in [".", "?", ":"]:
+                    print(char + "\n")
+                    i = 0
+                else:
+                    print(char, end="")
