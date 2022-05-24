4092556e46cbeafee8946580281e4085165f7270
comels <come.l.s@hotmail.fr>
2022-05-24 08:37:01 +0000

    first and last commit

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""the copy_list function
+"""
+
+
+def copy_list(l):
+    """function that copy a list
+
+    Args:
+        l (list): the list to copy
+
+    Returns:
+        list: the new list
+    """
+    new = l.copy()
+    return new
