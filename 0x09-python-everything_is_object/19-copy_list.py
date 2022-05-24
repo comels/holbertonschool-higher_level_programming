c2c50df46e519ffdedf96a3906f42c1e0bfe7494
comels <come.l.s@hotmail.fr>
2022-05-24 15:00:53 +0000

    19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-"""the copy_list function
-"""
-
-
 def copy_list(l):
-    """function that copy a list
-
-    Args:
-        l (list): the list to copy
-
-    Returns:
-        list: the new list
-    """
-    new = l.copy()
-    return new
+    return l.copy()
