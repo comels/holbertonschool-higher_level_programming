f20406e5158727c7181ec4ba634efa784f121776
comels <come.l.s@hotmail.fr>
2022-06-07 10:09:36 +0000

    up2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,7 +40,7 @@ class Square(Rectangle):
             if "id" in kwargs:
                 self.id = kwargs["id"]
             if "size" in kwargs:
-                self.height = kwargs["size"]
+                self.width = kwargs["size"]
             if "x" in kwargs:
                 self.x = kwargs["x"]
             if "y" in kwargs:
