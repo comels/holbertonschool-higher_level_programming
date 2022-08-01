e2e09edf73bad20e340ece660871ae707d1f5d07
Come Le Sauter <come.l.s@hotmail.fr>
2022-08-01 12:47:26 +0200

    101

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const tab = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    tab.push(parseInt(process.argv[i]));
+  }
+  tab.sort();
+  console.log(tab[tab.length - 2]);
+}
