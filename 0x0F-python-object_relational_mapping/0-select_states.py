f12ff6710ddc0f575f1763d45fdf6737ee1fda82
comels <come.l.s@hotmail.fr>
2022-08-07 21:22:42 +0000

    first

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd="come", db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id;")
     rows = cur.fetchall()
