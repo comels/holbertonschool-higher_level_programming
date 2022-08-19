49a802cd12b4c2de434d1d537f868e977ea6f020
comels <come.l.s@hotmail.fr>
2022-08-19 08:18:01 +0000

    second commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a
+request to the URL and displays the body
+of the response (decoded in utf-8)"""
+
+if __name__ == "__main__":
+
+    from urllib.request import urlopen
+    from sys import argv
+
+    try:      
+        with urlopen(argv[1]) as response:
+            print(response.read()) 
+    except urllib.error.HTTPError as e:     
+        print(e.reason)
