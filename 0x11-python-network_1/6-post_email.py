f34916dd1ca511958334de27c58ea810fa75c8d6
comels <come.l.s@hotmail.fr>
2022-08-19 08:57:17 +0000

    add

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that takes in a URL and
+an email, sends a POST request to the
+passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = {'email': argv[2]}
+
+    req = requests.post(url, json = email)
+
+    print(req.text)
